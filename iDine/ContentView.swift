fc555f10ad2696f650d84ceae89fb00ab61df6d6
Nick Thacke <ncthacke@gmail.com>
2023-07-08 17:32:12 -0400

    inital front end

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+enum ButtonState {
+    case breakfast
+    case lunch
+    case dinner
+    case unselected
+}
 struct ContentView: View {
+    @State private var buttonState : ButtonState = .unselected
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Spacer()
+                    Button(action : {
+                        
+                    }) {
+                        Image(systemName: "cart.circle")
+                            .resizable()
+                            .frame(width: 25, height:25)
+                    }
+                    .padding()
+                }
+                Image(systemName: "fork.knife.circle")
+                    .resizable()
+                    .padding()
+                    .frame(width:100, height:100)
+                Spacer()
+                HStack {
+                    Button("Breakfast", action : {
+                        buttonState = .breakfast
+                    })
+                    .foregroundColor(buttonState == .breakfast ? Color.red : Color.blue)
+                    Button("Lunch", action : {
+                        buttonState = .lunch
+                    })
+                    .foregroundColor(buttonState == .lunch ? Color.gray : Color.blue)
+                    Button("Dinner", action : {
+                        buttonState = .dinner
+                    })
+                    .foregroundColor(buttonState == .dinner ? Color.black : Color.blue)
+                }
+                
+                NavigationStack {
+                    
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
