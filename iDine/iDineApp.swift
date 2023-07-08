3b2c8d1cec91f075d3aa3fc77ec6d6a3c788f684
Nick Thacke <ncthacke@gmail.com>
2023-07-08 16:30:59 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Nick Thacke on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
